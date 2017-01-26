e7a934f49a87670c6a29528e94ac4fe7539a329c
Jill-Jênn Vie <vie@jill-jenn.net>
2017-01-26 18:12:12 +0900

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pixutils',
+      version='1.0',
+      description='Utils for performing analysis of PIX data',
+      author='Jill-Jênn Vie',
+      author_email='vie@jill-jenn.net',
+      url='https://github.com/jilljenn/pixutils/',
+      packages=['pixutils'],
+     )
