a3fe30b62b3396c65f7db9652483a0c5e94dc778
Jill-Jênn Vie <vie@jill-jenn.net>
2017-09-01 18:46:14 +0900

    Update challenges of adaptive courses

--- pixutils/airtable.py
+++ pixutils/airtable.py
@@ -1,4 +1,5 @@
 from settings import AIRTABLE_BASE, AIRTABLE_API_KEY
+from collections import defaultdict
 import requests
 import json
 import os
@@ -30,17 +31,28 @@ def lookup_challenges(query, offset=None):
         params['offset'] = offset
     response = make_request('/Épreuves', params)
     acquix = set()
+    comp_chall = set()
     if 'records' in response:
         print(len(response['records']), 'results')
         if 'offset' in response:
             print(response['offset'])
-            acquix.update(lookup_challenges(query, offset=response['offset']))
+            next_acquix, next_comp_chall = lookup_challenges(query, offset=response['offset'])
+            acquix.update(next_acquix)
+            comp_chall.update(next_comp_chall)
         for entry in response['records']:
+            challenge_id = entry['fields']['Record ID']
             acquis = entry['fields'].get('acquis')
             competence = entry['fields']['competences'][0]
+            comp_chall.add((competence, challenge_id))
             for skill in acquis:
                 level = int(skill[-1])
                 acquix.add((competence, level, skill))
     else:
         print(response)
-    return sorted(acquix)
+    return sorted(acquix), comp_chall
+
+def update_challenges(adaptive_course_id, challenge_ids):
+    new_test = {'fields': {'Épreuves': list(challenge_ids)[::-1]}}
+    r = requests.patch('https://api.airtable.com/v0/{}/Tests/%s'.format(AIRTABLE_BASE) % adaptive_course_id, headers={
+        'Authorization': 'Bearer {}'.format(AIRTABLE_API_KEY), 'Content-type': 'application/json'}, data=json.dumps(new_test))
+    print(r.json())
--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,4 @@
-from pixutils.airtable import make_request, lookup_challenges
+from pixutils.airtable import make_request, lookup_challenges, update_challenges
 from collections import Counter, defaultdict
 
 competences = {}
@@ -9,7 +9,8 @@ for entry in response['records']:
     titles[entry['fields']['Sous-domaine']] = entry['fields']['Titre']
 
 knowledge = defaultdict(lambda: defaultdict(set))
-acquix = lookup_challenges('')
+challengesOfCompetence = defaultdict(set)
+acquix, comp_chall = lookup_challenges('')
 for competence, level, skill in acquix:
     knowledge[competences[competence]][level].add(skill)
 
@@ -17,4 +18,29 @@ for competence in sorted(knowledge):
     print('#', competence, titles[competence])
     for level in sorted(knowledge[competence]):
         nb_skills = len(knowledge[competence][level])
-        print('Level {} : 40 pix pour {} acquix donc {:.2f} pix par skill, ex. {}'.format(level, nb_skills, 40 / nb_skills, sorted(list(knowledge[competence][level]))[:5]))
+        print('Level {} : 5 pix pour {} acquix donc {:.2f} pix par skill, ex. {}'.format(level, nb_skills, 5 / nb_skills, sorted(list(knowledge[competence][level]))[:5]))
+
+for competence_id, challenge_id in comp_chall:
+    challengesOfCompetence[competence_id].add(challenge_id)
+
+for competence_id in challengesOfCompetence:
+    print(competence_id, list(challengesOfCompetence[competence_id])[:5])
+
+keys = {
+    '1.1': ('recsvLz0W2ShyfD63', 'recNPB7dTNt5krlMA'),
+    '1.2': ('recIkYm646lrGvLNT', 'recAY0W7xurA11OLZ'),
+    '1.3': ('recNv8qhaY887jQb2', 'recR9yCEqgedB0LYQ'),
+    '3.1': ('recOdC9UDVJbAXHAm', 'rec43mpMIR5dUzdjh'),
+    '3.2': ('recbDTF8KwupqkeZ6', 'recVtTay20uxEqubF'),
+    '3.3': ('recHmIWG6D0huq6Kx', 'recRKkLdx99wfl3qs'),
+    '3.4': ('rece6jYwH4WEw549z', 'recTMfUJzFaNiUt64'),
+    '4.1': ('rec6rHqas39zvLZep', 'recO1qH39C0IfggLZ'), # 4.1 Sécuriser
+    '4.2': ('recofJCxg0NqTqTdP', 'recrJ90Sbrotzkb7x'), # 4.2
+    '4.3': ('recfr0ax8XrfvJ3ER', 'recRlIVstCemVM8jE'), # 4.3
+    '5.1': ('recIhdrmCuEmCDAzj', 'rec5gEPqhxYjz15eI'), # 5.1
+    '5.2': ('recudHE5Omrr10qrx', 'recfLYUy8fYlcyAsl'), # 5.2
+}
+
+for competence in ['5.1', '5.2', '1.1', '1.2', '1.3', '3.1', '3.2', '3.3', '3.4']:
+    competence_id, course_id = keys[competence]
+    update_challenges(course_id, challengesOfCompetence[competence_id])
